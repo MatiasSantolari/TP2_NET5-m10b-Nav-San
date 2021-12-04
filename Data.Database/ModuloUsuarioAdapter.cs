7c5643d0bef025d014a2c4cc2a6ad49add38f433
Matias <santolarimatias@gmail.com>
2021-12-04 16:10:45 -0300

    Modulo Desktop Hecho

---
+++ Data.Database/ModuloUsuarioAdapter.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+
+namespace Data.Database
+{
+    public class ModuloUsuarioAdapter: Adapter
+    {
+
+        public List<ModuloUsuario> GetAll()
+        {
+            List<ModuloUsuario> modsUs = new List<ModuloUsuario>();
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdModsUs = new SqlCommand("select * from modulos_usuarios", sqlConnection);
+                SqlDataReader drModsUs = cmdModsUs.ExecuteReader();
+                while (drModsUs.Read())
+                {
+                    ModuloUsuario mu = new ModuloUsuario();
+                    mu.ID = (int)drModsUs["id_modulo_usuario"];
+                    mu.IdModulo = (int)drModsUs["id_modulo"];
+                    mu.IdUsuario = (int)drModsUs["id_usuario"];
+                    mu.PermiteAlta = (bool)drModsUs["alta"];
+                    mu.PermiteBaja = (bool)drModsUs["baja"];
+                    mu.PermiteConsulta = (bool)drModsUs["consulta"];
+                    mu.PermiteModificacion= (bool)drModsUs["modificacion"];
+                    modsUs.Add(mu);
+                }
+                drModsUs.Close();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar lista de modulos_usuarios", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return modsUs;
+        }
+
+        public ModuloUsuario GetOne(int ID)
+        {
+            ModuloUsuario mu = new ModuloUsuario();
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdModsUs = new SqlCommand("Select * from modulos_usuarios where id_modulo_usuario = @id", sqlConnection);
+                cmdModsUs.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                SqlDataReader drModsUs = cmdModsUs.ExecuteReader();
+                if (drModsUs.Read())
+                {
+                    mu.ID = (int)drModsUs["id_modulo_usuario"];
+                    mu.IdModulo = (int)drModsUs["id_modulo"];
+                    mu.IdUsuario = (int)drModsUs["id_usuario"];
+                    mu.PermiteAlta = (bool)drModsUs["alta"];
+                    mu.PermiteBaja = (bool)drModsUs["baja"];
+                    mu.PermiteConsulta = (bool)drModsUs["consulta"];
+                    mu.PermiteModificacion = (bool)drModsUs["modificacion"];
+                }
+                drModsUs.Close();
+            }
+            catch (Exception e)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar datos del modulo_usuario", e);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return mu;
+        }
+        protected void Update(ModuloUsuario mu)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand("UPDATE modulos_usuarios SET id_modulo=@id_modulo, id_usuario=@id_usuario, alta=@alta, baja=@baja, modificacion=@modificacion, consulta=@consulta where id_modulo_usuario=@id", sqlConnection);
+                cmdSave.Parameters.Add("@id", SqlDbType.Int).Value = mu.ID;
+                cmdSave.Parameters.Add("@id_modulo", SqlDbType.Int).Value = mu.IdModulo;
+                cmdSave.Parameters.Add("@id_usuario", SqlDbType.Int).Value = mu.IdUsuario;
+                cmdSave.Parameters.Add("@alta", SqlDbType.TinyInt).Value = mu.PermiteAlta;
+                cmdSave.Parameters.Add("@baja", SqlDbType.TinyInt).Value = mu.PermiteBaja;
+                cmdSave.Parameters.Add("@modificacion", SqlDbType.TinyInt).Value = mu.PermiteModificacion;
+                cmdSave.Parameters.Add("@consulta", SqlDbType.TinyInt).Value = mu.PermiteConsulta;
+                cmdSave.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Exception er = new Exception("Error al actualizar los datos del modulo_usuario", e);
+                throw er;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        public void Delete(int ID)
+        {
+            try
+            {
+                //abrimos la conexión
+                this.OpenConnection();
+
+                //creamos la sentencia sql y asignamos un valor al parámetro
+                SqlCommand cmdDelete = new SqlCommand("delete modulos_usuarios where id_modulo_usuario=@id", sqlConnection);
+                cmdDelete.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                cmdDelete.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al eliminar el modulo_usuario", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+        protected void Insert(ModuloUsuario mu)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand(
+                "insert into modulos_usuarios(id_modulo,id_usuario,alta,baja,modificacion,consulta) " +
+                "Values(@id_modulo,@id_usuario,@alta,@baja,@modificacion,@consulta)", sqlConnection);
+                cmdSave.Parameters.Add("@id_modulo", SqlDbType.Int).Value = mu.IdModulo;
+                cmdSave.Parameters.Add("@id_usuario", SqlDbType.Int).Value = mu.IdUsuario;
+                cmdSave.Parameters.Add("@alta", SqlDbType.TinyInt).Value = mu.PermiteAlta;
+                cmdSave.Parameters.Add("@baja", SqlDbType.TinyInt).Value = mu.PermiteBaja;
+                cmdSave.Parameters.Add("@modificacion", SqlDbType.TinyInt).Value = mu.PermiteModificacion;
+                cmdSave.Parameters.Add("@consulta", SqlDbType.TinyInt).Value = mu.PermiteConsulta;
+
+                cmdSave.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al crear modulo_usuario", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        public void Save(ModuloUsuario mu)
+        {
+            if (mu.State == BusinessEntity.States.Deleted)
+            {
+                this.Delete(mu.ID);
+            }
+            else if (mu.State == BusinessEntity.States.New)
+            {
+                this.Insert(mu);
+            }
+            else if (mu.State == BusinessEntity.States.Modified)
+            {
+                this.Update(mu);
+            }
+            mu.State = BusinessEntity.States.Unmodified;
+        }
+
+    }
+}
---
+++ UI.Desktop/ModuloUsuarioForm.Designer.cs
@@ -0,0 +1,265 @@
+﻿
+namespace UI.Desktop
+{
+    partial class ModuloUsuarioForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModuloUsuarioForm));
+            this.tcModulosUsuarios = new System.Windows.Forms.ToolStripContainer();
+            this.tlModulosUsuarios = new System.Windows.Forms.TableLayoutPanel();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.dgvModulosUsuarios = new System.Windows.Forms.DataGridView();
+            this.tsModulosUsuarios = new System.Windows.Forms.ToolStrip();
+            this.tsbNuevo = new System.Windows.Forms.ToolStripButton();
+            this.tsbEditar = new System.Windows.Forms.ToolStripButton();
+            this.tsbEliminar = new System.Windows.Forms.ToolStripButton();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modulo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Alta = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Baja = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Modificacion = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Consulta = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.tcModulosUsuarios.ContentPanel.SuspendLayout();
+            this.tcModulosUsuarios.TopToolStripPanel.SuspendLayout();
+            this.tcModulosUsuarios.SuspendLayout();
+            this.tlModulosUsuarios.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvModulosUsuarios)).BeginInit();
+            this.tsModulosUsuarios.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tcModulosUsuarios
+            // 
+            // 
+            // tcModulosUsuarios.ContentPanel
+            // 
+            this.tcModulosUsuarios.ContentPanel.Controls.Add(this.tlModulosUsuarios);
+            this.tcModulosUsuarios.ContentPanel.Size = new System.Drawing.Size(716, 298);
+            this.tcModulosUsuarios.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcModulosUsuarios.Location = new System.Drawing.Point(0, 0);
+            this.tcModulosUsuarios.Name = "tcModulosUsuarios";
+            this.tcModulosUsuarios.Size = new System.Drawing.Size(716, 323);
+            this.tcModulosUsuarios.TabIndex = 0;
+            this.tcModulosUsuarios.Text = "toolStripContainer1";
+            // 
+            // tcModulosUsuarios.TopToolStripPanel
+            // 
+            this.tcModulosUsuarios.TopToolStripPanel.Controls.Add(this.tsModulosUsuarios);
+            // 
+            // tlModulosUsuarios
+            // 
+            this.tlModulosUsuarios.ColumnCount = 2;
+            this.tlModulosUsuarios.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlModulosUsuarios.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tlModulosUsuarios.Controls.Add(this.btnActualizar, 0, 1);
+            this.tlModulosUsuarios.Controls.Add(this.btnSalir, 1, 1);
+            this.tlModulosUsuarios.Controls.Add(this.dgvModulosUsuarios, 0, 0);
+            this.tlModulosUsuarios.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlModulosUsuarios.Location = new System.Drawing.Point(0, 0);
+            this.tlModulosUsuarios.Name = "tlModulosUsuarios";
+            this.tlModulosUsuarios.RowCount = 2;
+            this.tlModulosUsuarios.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlModulosUsuarios.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlModulosUsuarios.Size = new System.Drawing.Size(716, 298);
+            this.tlModulosUsuarios.TabIndex = 0;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnActualizar.Location = new System.Drawing.Point(557, 272);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 0;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(638, 272);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 1;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // dgvModulosUsuarios
+            // 
+            this.dgvModulosUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvModulosUsuarios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Modulo,
+            this.Usuario,
+            this.Alta,
+            this.Baja,
+            this.Modificacion,
+            this.Consulta});
+            this.tlModulosUsuarios.SetColumnSpan(this.dgvModulosUsuarios, 2);
+            this.dgvModulosUsuarios.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvModulosUsuarios.Location = new System.Drawing.Point(3, 3);
+            this.dgvModulosUsuarios.Name = "dgvModulosUsuarios";
+            this.dgvModulosUsuarios.Size = new System.Drawing.Size(710, 263);
+            this.dgvModulosUsuarios.TabIndex = 2;
+            // 
+            // tsModulosUsuarios
+            // 
+            this.tsModulosUsuarios.Dock = System.Windows.Forms.DockStyle.None;
+            this.tsModulosUsuarios.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbNuevo,
+            this.tsbEditar,
+            this.tsbEliminar});
+            this.tsModulosUsuarios.Location = new System.Drawing.Point(3, 0);
+            this.tsModulosUsuarios.Name = "tsModulosUsuarios";
+            this.tsModulosUsuarios.Size = new System.Drawing.Size(81, 25);
+            this.tsModulosUsuarios.TabIndex = 0;
+            // 
+            // tsbNuevo
+            // 
+            this.tsbNuevo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbNuevo.Image = ((System.Drawing.Image)(resources.GetObject("tsbNuevo.Image")));
+            this.tsbNuevo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbNuevo.Name = "tsbNuevo";
+            this.tsbNuevo.Size = new System.Drawing.Size(23, 22);
+            this.tsbNuevo.Text = "toolStripButton1";
+            this.tsbNuevo.ToolTipText = "Nuevo";
+            this.tsbNuevo.Click += new System.EventHandler(this.tsbNuevo_Click);
+            // 
+            // tsbEditar
+            // 
+            this.tsbEditar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbEditar.Image = ((System.Drawing.Image)(resources.GetObject("tsbEditar.Image")));
+            this.tsbEditar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbEditar.Name = "tsbEditar";
+            this.tsbEditar.Size = new System.Drawing.Size(23, 22);
+            this.tsbEditar.Text = "toolStripButton2";
+            this.tsbEditar.ToolTipText = "Editar";
+            this.tsbEditar.Click += new System.EventHandler(this.tsbEditar_Click);
+            // 
+            // tsbEliminar
+            // 
+            this.tsbEliminar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbEliminar.Image = ((System.Drawing.Image)(resources.GetObject("tsbEliminar.Image")));
+            this.tsbEliminar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbEliminar.Name = "tsbEliminar";
+            this.tsbEliminar.Size = new System.Drawing.Size(23, 22);
+            this.tsbEliminar.ToolTipText = "Eliminar";
+            this.tsbEliminar.Click += new System.EventHandler(this.tsbEliminar_Click);
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // Modulo
+            // 
+            this.Modulo.DataPropertyName = "IdModulo";
+            this.Modulo.HeaderText = "Modulo";
+            this.Modulo.Name = "Modulo";
+            this.Modulo.ReadOnly = true;
+            // 
+            // Usuario
+            // 
+            this.Usuario.DataPropertyName = "IdUsuario";
+            this.Usuario.HeaderText = "Usuario";
+            this.Usuario.Name = "Usuario";
+            this.Usuario.ReadOnly = true;
+            // 
+            // Alta
+            // 
+            this.Alta.DataPropertyName = "PermiteAlta";
+            this.Alta.HeaderText = "Alta";
+            this.Alta.Name = "Alta";
+            this.Alta.ReadOnly = true;
+            // 
+            // Baja
+            // 
+            this.Baja.DataPropertyName = "PermiteBaja";
+            this.Baja.HeaderText = "Baja";
+            this.Baja.Name = "Baja";
+            this.Baja.ReadOnly = true;
+            // 
+            // Modificacion
+            // 
+            this.Modificacion.DataPropertyName = "PermiteModificacion";
+            this.Modificacion.HeaderText = "Modificacion";
+            this.Modificacion.Name = "Modificacion";
+            this.Modificacion.ReadOnly = true;
+            // 
+            // Consulta
+            // 
+            this.Consulta.DataPropertyName = "PermiteConsulta";
+            this.Consulta.HeaderText = "Consulta";
+            this.Consulta.Name = "Consulta";
+            this.Consulta.ReadOnly = true;
+            // 
+            // ModuloUsuarioForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 323);
+            this.Controls.Add(this.tcModulosUsuarios);
+            this.Name = "ModuloUsuarioForm";
+            this.Text = "ModuloUsuarioForm";
+            this.Load += new System.EventHandler(this.ModuloUsuarioForm_Load);
+            this.tcModulosUsuarios.ContentPanel.ResumeLayout(false);
+            this.tcModulosUsuarios.TopToolStripPanel.ResumeLayout(false);
+            this.tcModulosUsuarios.TopToolStripPanel.PerformLayout();
+            this.tcModulosUsuarios.ResumeLayout(false);
+            this.tcModulosUsuarios.PerformLayout();
+            this.tlModulosUsuarios.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvModulosUsuarios)).EndInit();
+            this.tsModulosUsuarios.ResumeLayout(false);
+            this.tsModulosUsuarios.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer tcModulosUsuarios;
+        private System.Windows.Forms.ToolStrip tsModulosUsuarios;
+        private System.Windows.Forms.TableLayoutPanel tlModulosUsuarios;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.DataGridView dgvModulosUsuarios;
+        private System.Windows.Forms.ToolStripButton tsbNuevo;
+        private System.Windows.Forms.ToolStripButton tsbEditar;
+        private System.Windows.Forms.ToolStripButton tsbEliminar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Modulo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Usuario;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Alta;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Baja;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Modificacion;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Consulta;
+    }
+}
